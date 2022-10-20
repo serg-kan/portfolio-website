03859fe8acb690259aa7ab9a48146671ab0382d7
Sergey Kan <serg.kan096@gmail.com>
2022-10-20 20:12:47 +0300

    fix: favicon

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,8 +5,8 @@ import CV from '../../assets/cv.pdf';
 const CTA = () => {
   return (
     <div className='cta'>
-        {/* <a href={CV} download className='btn'>Download CV</a>  */}
-        <a href="https://t.me/product_kan" target="_blank" rel="noreferrer" className='btn'>Telegram Channel</a> 
+        <a href={CV} download className='btn'>Download CV</a> 
+        {/* <a href="https://t.me/product_kan" target="_blank" rel="noreferrer" className='btn'>Telegram Channel</a>  */}
         <a href="#contact" className='btn btn-primary'>Contact me</a>
     </div>
   )
